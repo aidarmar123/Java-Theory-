e32f43d8c5a9c937cd05c99948ec523f880dd34e
Aidar <ajdar.mar@yandex.ru>
2024-11-12 19:12:10 +0300

    Add code

--- Multithereading/src/Main.java
+++ Multithereading/src/Main.java
@@ -1,15 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        Thread task1 = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("Thread -1: "+i);
+            }
+        });
+        Thread task2 = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("Thread -2: "+i);
+            }
+        });
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        task2.setName("Deamon thread");
+        task2.setDaemon(true); // Фоновый поток
+
+        task1.start(); //Запуск первого потока
+        task2.start();
+
+
+
+        task1.join(); //Ожидание пока поток не закочиться
+        System.out.println("Thread is finish");
+
+
     }
 }
